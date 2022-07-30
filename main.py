f37113cd86622255db5ed302756c4820bbd47db9
庭庭 <qsid1130@gmail.com>
2022-07-30 20:01:16 +0800

    Changes while to for

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 x = [1, 2, 3, 4, 5]
 
-i = 0
 m = -999999
-while i < len(x):
-    if x[i] > m:
-        m = x[i]
-    i = i + 1
+for current in x:
+    if current > m:
+        m = current
 
 print(m)
