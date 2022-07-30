35591ab80ad43366cefd0624bc5b86c8ab53cfc2
庭庭 <qsid1130@gmail.com>
2022-07-30 23:58:52 +0800

    left rotation

---
+++ functions/left_rotation.py
@@ -0,0 +1,17 @@
+def left_rotation(arr, times):
+    i = 0
+    while i < times:
+        x = arr[0]
+        for j in range(0, len(arr) - 1):
+            arr[j] = arr[j + 1]
+        i = i + 1
+        arr[len(arr) - 1] = x
+
+
+
+if __name__ == "__main__":
+    arr = [1, 5, 3, 9, 2]
+    left_rotation(arr, 1)
+    print(arr)
+    left_rotation(arr, 3)
+    print(arr)
