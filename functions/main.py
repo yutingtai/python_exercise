cdbdd8a1d2e5615c5f869a8cf8543f114b37d29c
庭庭 <qsid1130@gmail.com>
2022-07-30 20:57:06 +0800

    name function

---
+++ functions/main.py
@@ -0,0 +1,27 @@
+name = "Aradi Patrik"
+age = 27
+
+print("I am {}, and I'm {} years old".format(name, age))
+if age < 40:
+    print("I am pretty young")
+else:
+    print("I am pretty old")
+
+name = "Nagy Péter"
+age = 52
+
+print("I am {}, and I'm {} years old".format(name, age))
+if age < 40:
+    print("I am pretty young")
+else:
+    print("I am pretty old")
+
+name = "Kis Pista"
+age = 21
+
+print("I am {}, and I'm {} years old".format(name, age))
+if age < 40:
+    print("I am pretty young")
+else:
+    print("I am pretty old")
+
---
+++ functions/names_function.py
@@ -0,0 +1,11 @@
+def print_intro(name, age):
+    print(f"I'm {name},and I'm {age}. ")
+    if age > 70:
+        print("I'm really old")
+    else:
+        print("I'm still young")
+
+
+print_intro('Tai Yu-Ting', 25)
+print_intro('Aradi Patrik', 77)
+
