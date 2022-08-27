157cb055cbb72bdcb681fcc7e9ec9ae1bc3f0564
庭庭 <qsid1130@gmail.com>
2022-08-27 13:59:22 +0800

    print the number of times that the substring occurs in the given string.

---
+++ HackerRank/count_substring.py
@@ -0,0 +1,19 @@
+def count_substring(string, sub_string):
+    string = string
+    sub_string = sub_string
+    len_s = len(sub_string)
+    i = 0
+    j = 0
+    while i < len(string):
+        if string[0+i:len_s+i] == sub_string:
+            j += 1
+        i += 1
+    return j
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
